d3142896dc786e12f8f44f95b2bc8dd1558378c5
Jaspreet000 <89904849+Jaspreet000@users.noreply.github.com>
2021-09-25 10:31:03 +0530

    Update userbotjoin.py

--- handlers/userbotjoin.py
+++ handlers/userbotjoin.py
@@ -35,7 +35,7 @@ async def addchannel(client, message):
     except Exception as e:
         print(e)
         await message.reply_text(
-            f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your group due to heavy join requests for userbot! Make sure user is not banned in group for more info contact @stylishuser."
+            f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your group due to heavy join requests for userbot! Make sure user is not banned in group for more info contact @jass3212."
             "\n\nOr manually add Asisstant to your Group and try again</b>",
         )
         return
@@ -110,7 +110,7 @@ async def addcchannel(client, message):
     except Exception as e:
         print(e)
         await message.reply_text(
-            f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your channel due to heavy join requests for userbot! Make sure user is not banned in channel for more info join @stylishuser."
+            f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your channel due to heavy join requests for userbot! Make sure user is not banned in channel for more info join @jass3212."
             f"\n\nOr manually add @{ASSISTANT_NAME} to your Group and try again</b>",
         )
         return
